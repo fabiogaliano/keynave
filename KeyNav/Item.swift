6e340e3a2ca0b5c0e31b59ec8ea42742cd7a8f85
Fábio Galiano <48589369+fabiogaliano@users.noreply.github.com>
2025-11-17 05:37:29 +0000

    Initial Commit

---
+++ KeyNav/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KeyNav
+//
+//  Created by fábio on 17/11/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
